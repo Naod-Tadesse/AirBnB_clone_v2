83eaac786880ce9297f074fdaca3372739df0345
Naod Taddese <naodtds@gmail.com>
2024-03-07 15:06:46 +0300

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""this extracts to web server
+"""
+import os
+from fabric.api import *
+
+env.hosts ['54.159.26.191', '54.157.182.105']
+
+def do_deploy(archive_path):
+    """this function distributes the file"""
+    if os.path.exists(archive_path):
+        try:
+            filename = archive_path.split('/')[-1]
+            direc = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            fname = f'/data/web_static/releases/{direc}'
+            run(f"mkdir -p /data/web_static/releases/{direc}")
+            run(f'tar -xzf /tmp/{filename} -C {fname}')
+            run(f'rm /tmp/{filename}')
+            run(f'rm -f /data/web_static/current')
+            run(f'sudo ln -sf {fname}/ /data/web_static/current')
+            return True
+        except Exception as e:
+            return False
+    else:
+        return False
