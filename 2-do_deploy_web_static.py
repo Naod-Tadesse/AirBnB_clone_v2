0bca3fa9886e6ed678940d6c2b2876b5c4d133ff
Naod Taddese <naodtds@gmail.com>
2024-03-07 18:40:51 +0300

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,22 +8,40 @@ env.hosts = ['54.159.26.191', '54.157.182.105']
 
 
 def do_deploy(archive_path):
-    """this function distributes the file"""
-    if os.path.exists(archive_path):
-        try:
-            filename = archive_path.split('/')[-1]
-            direc = filename.split('.')[0]
-            put(archive_path, '/tmp/')
-            fname = f'/data/web_static/releases/{direc}'
-            run(f"sudo mkdir -p /data/web_static/releases/{direc}/")
-            run(f'sudo tar -xzf /tmp/{filename} -C {fname}')
-            run(f'sudo rm /tmp/{filename}')
-            run(f'sudo mv {fname}/web_static/* {fname}/')
-            run(f'sudo rm -rf {fname}/web_static')
-            run(f'sudo rm -rf /data/web_static/current')
-            run(f'sudo ln -sf {fname}/ /data/web_static/current')
-            return True
-        except Exception as e:
+    """files to deploy"""
+    try:
+        print("Starting do_deploy function...")
+        if not (path.exists(archive_path)):
             return False
-    else:
+
+        put(archive_path, "/tmp/")
+
+        # uncompress the archive
+        parts = archive_path.split('_')
+        time_stamp = parts[2].split(".")[0]
+        run("sudo rm -rf /data/web_static/releases/\
+web_static_{}".format(time_stamp))
+        run("sudo mkdir -p /data/web_static/releases/\
+web_static_{}".format(time_stamp))
+        run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/\
+web_static/releases/web_static_{}".format(time_stamp, time_stamp))
+
+        # delete the archive from web_server
+        run("sudo rm -rf /tmp/web_static_{}.tgz".format(time_stamp))
+
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(time_stamp, time_stamp))
+
+        # delete the file
+        run("sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static".format(time_stamp))
+        # delete the symbolic link
+        run("sudo rm -rf /data/web_static/current")
+
+        # create a new symblolic link
+        run("sudo ln -sf /data/web_static/releases/\
+web_static_{} /data/web_static/current".format(time_stamp))
+
+        return True
+    except Exception as e:
         return False
