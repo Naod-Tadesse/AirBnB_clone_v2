fd9446f94ca67167dc7d4d6bb03e119979d05885
Naod Taddese <naodtds@gmail.com>
2024-03-07 17:43:17 +0300

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,20 +9,21 @@ env.hosts = ['54.159.26.191', '54.157.182.105']
 
 def do_deploy(archive_path):
     """this function distributes the file"""
-    try:
-        if not os.path.exists(archive_path):
+    if os.path.exists(archive_path):
+        try:
+            filename = archive_path.split('/')[-1]
+            direc = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            fname = f'/data/web_static/releases/{direc}'
+            run(f"sudo mkdir -p /data/web_static/releases/{direc}/")
+            run(f'sudo tar -xzf /tmp/{filename} -C {fname}')
+            run(f'sudo rm /tmp/{filename}')
+            run(f'sudo mv {fname}/web_static/* {fname}/')
+            run(f'sudo rm -rf {fname}/web_static')
+            run(f'sudo rm -rf /data/web_static/current')
+            run(f'sudo ln -sf {fname}/ /data/web_static/current')
+            return True
+        except Exception as e:
             return False
-        filename = archive_path.split('/')[-1]
-        direc = filename.split('.')[0]
-        put(archive_path, '/tmp/')
-        fname = f'/data/web_static/releases/{direc}'
-        run(f"sudo mkdir -p /data/web_static/releases/{direc}/")
-        run(f'sudo tar -xzf /tmp/{filename} -C {fname}')
-        run(f'sudo rm /tmp/{filename}')
-        run(f'sudo mv {fname}/web_static/* {fname}/')
-        run(f'sudo rm -rf {fname}/web_static')
-        run(f'sudo rm -rf /data/web_static/current')
-        run(f'sudo ln -sf {fname}/ /data/web_static/current')
-        return True
-    except Exception as e:
+    else:
         return False
