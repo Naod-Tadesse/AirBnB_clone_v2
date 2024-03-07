bee5f11b8c424fe6e43c899179a22c0db5dc32c9
Naod Taddese <naodtds@gmail.com>
2024-03-07 13:56:49 +0300

    task 1 done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""create archive from webstatic"""
+from datetime import datetime
+
+
+def do_pack():
+    fname = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}'
+    local("mkdir -p versions/")
+    save = local(f"tar -cvzf versions/{fname} web_static")
+    if not result.failed:
+        return f"versions/fname"
+    else:
+        return None
