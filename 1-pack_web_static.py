5acfefaa0ac45062d99a810ee913ab2ab8ef3b65
Naod Taddese <naodtds@gmail.com>
2024-03-07 14:00:20 +0300

    fixes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ def do_pack():
     fname = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}'
     local("mkdir -p versions/")
     save = local(f"tar -cvzf versions/{fname} web_static")
-    if not result.failed:
-        return f"versions/fname"
+    if not save.failed:
+        return f"versions/{fname}"
     else:
         return None
