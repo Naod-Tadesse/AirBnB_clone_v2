600126e7df5e55d58c88bf5c96d26a9e487cdc66
Naod Taddese <naodtds@gmail.com>
2024-03-07 14:10:21 +0300

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from fabric.api import local
 from time import strftime
 
+
 def do_pack():
     """create the zip file"""
-    tim = datetime.now()
-    fname = f'web_static_{tim.strftime("%Y%m%d%H%M%S")}'
+    fname = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}'
     local("mkdir -p versions/")
-    save = local(f"tar -cvzf versions/{fname} web_static")
+    save = local(f"tar -cvzf versions/{fname}.tgz web_static")
     if not save.failed:
         return f"versions/{fname}"
     else:
