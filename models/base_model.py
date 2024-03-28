283d575022989e46b5e0b399dd947189d158c3bd
Naod Taddese <naodtds@gmail.com>
2024-03-28 12:37:48 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,21 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
+            else:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+            else:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
     def __str__(self):
