355e61ca922d84aeedfffbca62fd841848461adb
Naod Taddese <naodtds@gmail.com>
2024-03-28 14:17:17 +0300

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""flash app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """route hello controller"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """route hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """c route"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def p_route(text):
+    """p route"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
