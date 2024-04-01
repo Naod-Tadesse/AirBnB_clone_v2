fb04f54d38ab202d7567ad9ecb8bafea16157f3e
Naod Taddese <naodtds@gmail.com>
2024-04-01 09:38:00 +0300

    done

--- models/state.py
+++ models/state.py
@@ -8,17 +8,18 @@ import os
 
 class State(BaseModel, Base):
     """ State class """
-    if os.getenv("HBNB_TYPE_STORAGE") == "file":
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City", backref="state", cascade="all, delete-orphan")
+    else:
         @property
         def cities(self):
             """get cities"""
+            from models import storage
             result = [
                 city for city in storage.all("City").values()
                 if city.state_id == self.id
             ]
             return result
-    else:
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            "City", backref="state", cascade="all, delete-orphan")
